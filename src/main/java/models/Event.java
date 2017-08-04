5ea04b6b4aef4f83dd2801a604cea09ff3488aca
jessicakenney <jessicakenney@yahoo.com>
2017-08-04 09:48:38 -0700

    first getter test failing for getNumPeople

--- src/main/java/models/Event.java
+++ src/main/java/models/Event.java
@@ -1,9 +1,29 @@
 package models;
 
 public class Event {
+  private Integer numPeople;
+  private String foodChoice;
+  private String beverageChoice;
+  private String entertainmentChoice;
 
   public Event( Integer numPeople, String foodChoice, String beverageChoice, String entertainmentChoice){
+    this.numPeople = numPeople;
+    this.foodChoice = foodChoice;
+    this.beverageChoice = beverageChoice;
+    this.entertainmentChoice = entertainmentChoice;
+  }
 
+  public Integer getNumPeople () {
+    return 0;
+  }
+  public String getFoodChoice () {
+    return "";
+  }
+  public String getBeverageChoice () {
+    return "";
+  }
+  public String getEntertainmentChoice () {
+    return "";
   }
 
 
--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -10,6 +10,12 @@ public class EventTest {
   public void newEvent_instantiatesCorrectly() throws Exception {
     Event testEvent = new Event(50, "lightsnacks", "nonalcoholic", "none");
     assertEquals(true, testEvent instanceof Event);
-
   }
+  @Test
+  public void newTest_getsNumPeople_Integer() throws Exception{
+    Event testEvent = new Event(50, "lightsnacks", "nonalcoholic", "none");
+    Integer expected = 50;
+    assertEquals(expected, testEvent.getNumPeople());
+  }
+
 }
