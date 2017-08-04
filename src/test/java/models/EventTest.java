e9f5132ae5a225678e0898f05b0385860d52f552
jessicakenney <jessicakenney@yahoo.com>
2017-08-04 09:52:53 -0700

    all getter test failing for newEvent Test

--- src/test/java/models/EventTest.java
+++ src/test/java/models/EventTest.java
@@ -17,5 +17,23 @@ public class EventTest {
     Integer expected = 50;
     assertEquals(expected, testEvent.getNumPeople());
   }
+  @Test
+  public void newTest_getsFoodChoice_String() throws Exception{
+    Event testEvent = new Event(50, "lightsnacks", "nonalcoholic", "none");
+    String expected = "lightsnacks";
+    assertEquals(expected, testEvent.getFoodChoice());
+  }
+  @Test
+  public void newTest_getsBeverageChoice_String() throws Exception{
+    Event testEvent = new Event(50, "lightsnacks", "nonalcoholic", "none");
+    String expected = "nonalcoholic";
+    assertEquals(expected, testEvent.getBeverageChoice());
+  }
+  @Test
+  public void newTest_getsEntertainmentChoice_String() throws Exception{
+    Event testEvent = new Event(50, "lightsnacks", "nonalcoholic", "none");
+    String expected = "none";
+    assertEquals(expected, testEvent.getEntertainmentChoice());
+  }
 
 }
